047738b0109c7cd9dda2487d6d0eb4f2f27c09d8
The Needful DO <the.needful.do@gmail.com>
2025-09-29 11:09:41 -0700

    Style wallet watch token checkbox as toggle switch

--- src/pawndao_frontend/src/lib/components/WalletBar.svelte
+++ src/pawndao_frontend/src/lib/components/WalletBar.svelte
@@ -638,7 +638,7 @@ $effect(() => {
         {#each wallet.watched_icrc1_tokens as token}
           <li class="py-1 flex items-center justify-end space-x-1">
             {#if isWalletBarEdit}
-              <input class="checkbox checkbox-info checkbox-xs" type="checkbox" bind:checked={token.watched} />
+              <input class="toggle toggle-info checkbox-xs" type="checkbox" bind:checked={token.watched} />
             {/if}
             <span>{token.symbol || token.canister_id}: {token.balance }</span>
             <button class={["rounded-full btn btn-info btn-xs btn-outline"]}
@@ -671,7 +671,7 @@ $effect(() => {
             <!--     <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8M1.173 8a13 13 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5s3.879 1.168 5.168 2.457A13 13 0 0 1 14.828 8q-.086.13-.195.288c-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5s-3.879-1.168-5.168-2.457A13 13 0 0 1 1.172 8z"/> -->
             <!--     <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5M4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0"/> -->
             <!--   </svg>    -->
-            <input class="checkbox checkbox-info checkbox-xs" type="checkbox" bind:checked={token.watched} />
+            <input class="toggle toggle-info checkbox-xs" type="checkbox" bind:checked={token.watched} />
             <!-- </label> -->
             <span>{token.symbol || token.canister_id}: {token.balance }</span>
             <input class="btn btn-xs btn-error" 
